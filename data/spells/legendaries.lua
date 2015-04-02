42cef74a7f299335b71b5c0cd50ac6e039ba48a9
markd315 <markd315@gmail.com>
2015-04-02 01:11:58 -0400

    Card changes

--- data/spells/legendaries.lua
+++ data/spells/legendaries.lua
@@ -17,11 +17,11 @@ local spell = Card:createMinion(0, 'Aggrizar Frostrage', 12, 2048, 0, 'Freeze a 
 local spell = Card:createMinion(0, 'Annison Runebender', 7, 60, 0, 'All spells cost 3 more mana.', 60, 80);
 	spell.onCast = function() local source = this(); this():addGlobalAura('MANACOST',function() if this().card.type == 'spell' then return 3 else return 0 end end,false); end;
 	
-local spell = Card:createMinion(0, 'Kelth Bluelight', 8, 120, 0, 'Taunt.  Heal 50 health to a character.', 50, 95);
+local spell = Card:createMinion(0, 'Kelth Bluelight', 9, 120, 0, 'Taunt.  Heal 50 health to a character.', 50, 95);
 	spell.onCast = function() game:chooseTarget(character,function() this():heal(50); end); end;
 	spell.taunt = true;
 
-local spell = Card:createMinion(0,'Gwenyth Bladestar', 9, 60, 0, 'Deal 60 damage to all characters.', 60, 60);
+local spell = Card:createMinion(0,'Gwenyth Bladestar', 9, 256, 0, 'Deal 60 damage to all characters.', 60, 60);
 	spell.onCast = function() local source=this(); game:damageForEach(function() return character and this()~=source end, 'NORMAL', 60, source.owner); end;
 
 local spell = Card:createMinion(0, 'Andrejana Fasthoof', 'NATURE', 7, 64, 0, 'NATURE.  CHARGE.  DIVINE SHIELD.', 50, 75);
@@ -47,7 +47,7 @@ local spell = Card:createMinion(0,'Phoenix', 'FIRE', 9, 60, 0, 'FIRE.  On Death:
 	spell.onDeath = function() this().controller:drawCard('Phoenix'); end;
 	
 local spell = Card:createMinion(0, 'Thaurum Shadowmaw', 'DARK', 8, 120, 0, 'Consume a minion and gain its attack and health.', 10, 10);
-	spell.onCast = function()  local thaurum = this(); local hp; local atk; game:chooseTarget(minion,function() 
+	spell.onCast = function() local thaurum = this(); local hp; local atk; game:chooseTarget(minion,function() 
 	hp = this():getHealth(); 
 	atk = this():getAttack(); 
 	thaurum:gain(atk, hp);
--- data/spells/minions.lua
+++ data/spells/minions.lua
@@ -2,22 +2,29 @@ Card = luajava.bindClass("tcg.Card");
 
 local spell = Card:createMinion(0, 'Circumcised Badger', 4, 7, 0, 'Why...', 30, 39);
 
-local spell = Card:createMinion(0, 'Lumbering Ox', 6, 16, 0, 'COOLDOWN', 45, 100);
+local spell = Card:createMinion(0, 'Lumbering Ox', 6, 16, 0, 'COOLDOWN', 44, 100);
 	spell.cooldown = true;
 
 local spell = Card:createMinion(0, 'Slumbering Giant', 7, 64, 1, 'SLOW START', 120, 85);
 	spell.onCast = function()  this():freeze(2); end;
 	
-local spell = Card:createMinion(0, 'Blood Golem', 4, 4, 1, 'LIFESTEAL', 35, 25);
+local spell = Card:createMinion(0, 'Blood Golem', 4, 4, 1, 'LIFESTEAL', 34, 25);
 	spell.lifeSteal = true;
 	
 local spell = Card:createMinion(0, 'Schrodinger\'s Paramecium', 2, 0, 0, 'TOXIC SKIN.  DODGE: 50%', 10, 10);
 	spell.isToxic = true;
 	spell.dodgeChance = 0.5;
 	
-local spell = Card:createMinion(0, 'Shapeshifter', 5, 16, 0, 'Choose a type for this minion: FIRE, WATER, or NATURE.', 40, 40);
+local spell = Card:createMinion(0, 'Shapeshifter', 5, 16, 0, 'Secretly choose a type for this minion.', 38, 38);
 	spell.onCast = function() game:chooseOne(
+	"NORMAL", function() this():changeType('NORMAL'); end,
 	"FIRE", function() this():changeType('FIRE'); end,
 	"WATER", function() this():changeType('WATER'); end,
-	"NATURE", function() this():changeType('NATURE'); end
+	"NATURE", function() this():changeType('NATURE'); end,
+	"TIME", function() this():changeType('TIME'); end,
+	"LIGHT", function() this():changeType('LIGHT'); end,
+	"DARK", function() this():changeType('DARK'); end,
+	"ALLIANCE", function() this():changeType('ALLIANCE'); end,
+	"INDUSTRY", function() this():changeType('INDUSTRY'); end,
+	"SPIRIT", function() this():changeType('SPIRIT'); end
 	); end;
